f724ebc73e998a53f03b78dcb4d6e67961e7d1f8
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-06-30 13:22:38 -0300

    Post

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ const app = express() // Inicializadno o express e guardando o express inicializ
 
 app.listen('3000') // Está ouvindo a porta 3000
 
+// GET (Navegador só faz GET)
+
 app.route('/').get( (request, response) => response.send("Hello World, This is my first server response") ) // Está enviando para a porta 3000
 app.route('/sobre').get( (request, response) => response.send("Hello World, This is route /sobre") )        // Está enviando para a porta 3000
+
+// Middleware primeiro passa por ele e após executa o verbo necesario
+
+app.use( express.json() ) // Transformar tudo em json
+
+// POST
+
+app.route('/').post( (request, response) => response.send(request.body)); // console.log(request.body) para ver no console do vsCode. response.send(request.body) retorna no insomnia o corpo da request enviada
