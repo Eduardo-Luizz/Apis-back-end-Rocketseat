d6f00ab21eb5a353566f544c07bf747586ddab3c
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-06-30 13:09:26 -0300

    Get

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const express = require('express')
 const app = express() // Inicializadno o express e guardando o express inicializado na variável app
 
 app.listen('3000') // Está ouvindo a porta 3000
+
+app.route('/').get( (request, response) => response.send("Hello World, This is my first server response") ) // Está enviando para a porta 3000
+app.route('/sobre').get( (request, response) => response.send("Hello World, This is route /sobre") )        // Está enviando para a porta 3000
