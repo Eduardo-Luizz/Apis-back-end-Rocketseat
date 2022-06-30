9e35588a6745c692137def0f75cf2c55d24e1b20
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-06-30 13:02:13 -0300

    Criando server

---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require('express') 
+const app = express() // Inicializadno o express e guardando o express inicializado na variável app
+
+app.listen('3000') // Está ouvindo a porta 3000
